e47d2f759ca3f1431648f2d23f4f0fad9f8af98d
gowthamanv1992 <g.vivekanandan@cranfield.ac.uk>
2021-04-27 01:50:45 +0100

    Added requirements.txt

--- simulator/settings.py
+++ simulator/settings.py
@@ -94,7 +94,7 @@ WSGI_APPLICATION = 'simulator.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'simulation',
         'USER': 'postgres',
         'PASSWORD': 'password',
